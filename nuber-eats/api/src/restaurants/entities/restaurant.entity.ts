8732f4b27d845cb2fdb4ae639544cee858c1f07f
m00p1ng <mongkonchai4412@gmail.com>
2022-03-18 23:25:39 +0700

    add restaurant

---
+++ nuber-eats/api/src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,40 @@
+import { Field, ObjectType, InputType } from '@nestjs/graphql';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+import { IsString, Length, IsBoolean, IsOptional } from 'class-validator';
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field(() => Boolean, { nullable: true, defaultValue: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  address: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  categoryName: string;
+}
